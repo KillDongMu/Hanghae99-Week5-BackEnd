ddb7a4d037628701bec3ed3faac5fa84889a54a7
matthewsmkim <forme3328@naver.com>
2022-10-21 16:35:36 +0900

    First Commit.

---
+++ src/main/java/com/killdongmu/Hanghae99Week5BackEnd/Hanghae99Week5BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.killdongmu.Hanghae99Week5BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghae99Week5BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hanghae99Week5BackEndApplication.class, args);
+	}
+
+}
